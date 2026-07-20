89084a0faed92ef249c02145a8385f20f05f95e7
raj-830 <rajarshimu@google.com>
2026-07-20 20:57:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,4 @@
-# --- OUTPUTS ---
-
 output "workflow_execution_id" {
-  description = "The tracking Execution ID generated in GCP"
-  value       = jsondecode(terracurl_request.trigger_workflow.response).name
+  description = "The dynamic execution path locator allocated by the Google Cloud Workflows API."
+  value       = jsondecode(data.http.trigger_workflow.response_body).name
 }
-
-# output "workflow_final_state" {
-#   description = "The status returned from the API"
-#   value       = jsondecode(data.http.workflow_status.response_body).state
-# }
-
-# output "workflow_output_payload" {
-#   description = "The actual JSON payload returned from the workflow execution steps"
-#   value       = jsondecode(data.http.workflow_status.response_body).result
-# }
